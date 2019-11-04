308bb24fd324ca7d83969534a4b56a88a739a6dd
Jasyn Marais <jasynmarais@gmail.com>
2019-11-04 15:24:27 +0200

    Add onClick handler to Away Team functions.

--- src/App.js
+++ src/App.js
@@ -44,8 +44,18 @@ const [awayScore, setAwayScore] = useState(0);
           </button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button 
+            className="awayButtons__touchdown"
+            onClick={() => setAwayScore(awayScore + 7)}
+            >
+              Away Touchdown
+          </button>
+          <button 
+          className="awayButtons__fieldGoal"
+          onClick={() => setAwayScore(awayScore + 3)}
+          >
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
